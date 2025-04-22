89fe38c3638ba4d012adc3707ed9f02622697604
Alex Proskuriakov <alexpro1972@yandex.ru>
2025-04-22 06:39:03 +0300

    Add extra models

--- src/pre_load/models.py
+++ src/pre_load/models.py
@@ -1,22 +1,24 @@
-import factory
+import factory as f
 
 from src.auth.services.password import hash_pwd
 from src.models import User
 
-fake = factory.Faker
+fake = f.Faker
 # from faker import Faker
 # Faker.phone_number()
 
 
-class UserFactory(factory.Factory):
+class UserFactory(f.Factory):
     class Meta:
         model = User
 
-    email = fake("email")
+    # first_name = fake("first_name")
+    # email = fake("email")
+    first_name = f.Sequence(lambda n: f"Name_{n}")
+    email = f.LazyAttribute(lambda _self: f"{_self.first_name.lower()}@example.com")
     hashed_pwd = hash_pwd(str(fake("password")))
-    first_name = fake("first_name")
     last_name = fake("last_name")
-    phone_number = fake("phone_number")
+    phone_number = fake("msisdn")
 
 
 # Another, different, factory for the same object
@@ -27,6 +29,24 @@ class AdminFactory(UserFactory):
     admin = True
 
 
-# admins = AdminFactory.create_batch(size=3)
-# from pprint import pprint
-# pprint(admins)
+"""
+import factory as f
+from factory.django import DjangoModelFactory
+from users.models import User
+
+
+class UserFactory(DjangoModelFactory):
+    class Meta:
+        model = User
+
+    username = f.Sequence(lambda n: f"Username_{n}")
+    email = f.LazyAttribute(lambda _self: f"{_self.username.lower()}@example.com")
+    first_name = f.Faker("first_name")
+    last_name = f.Faker("last_name")
+    password = f.Faker("password")
+    mobilefone = f.Faker("msisdn")
+    workplace = f.Sequence(lambda n: f"Место работы №{n}")
+    position = f.Faker("job")
+    experience = f.Iterator(User.EXPERIENCE_CHOICES)
+
+"""
